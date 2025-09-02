eb739b158e80adea9300e6d46f7959b2b7b3082b
dashygo097 <dashygo097@gmail.com>
2025-09-02 17:28:11 +0800

    add waveform bar

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 
 import { withRouter } from "../utils/withRouter";
+import Waveform from "../components/Waveform";
+
+const MAX_POINTS = 100;
 
 interface HomePageProps {
   navigate: (path: string) => void;
 }
 
 class HomePage extends React.Component<HomePageProps> {
+  timerId?: NodeJS.Timeout;
   constructor(props: HomePageProps) {
     super(props);
   }
@@ -14,8 +18,8 @@ class HomePage extends React.Component<HomePageProps> {
   render() {
     return (
       <div className="page-home">
-        <h1>Welcome Home!</h1>
-        <p style={{ fontSize: "1.1rem", maxWidth: "600px" }}>Welcome!</p>
+        <h1>Waveform Example</h1>
+        <Waveform data={[]} />
       </div>
     );
   }
