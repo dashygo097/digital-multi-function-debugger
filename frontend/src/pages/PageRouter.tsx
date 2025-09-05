e4b41712161b0232f56bccc0b7fae4e7904223f5
dashygo097 <dashygo097@github.com>
2025-09-05 14:45:11 +0800

    add github icon

--- frontend/src/pages/PageRouter.tsx
+++ frontend/src/pages/PageRouter.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import WrappedNavBar from "../components/NavBar";
-import WrappedHomePage from "./HomePage";
+import WrappedMainPage from "./MainPage";
 import WrappedAboutPage from "./AboutPage";
 import WrappedNotFound from "./NotFound";
+import WrappedDocPage from "./DocPage";
 
 const PageRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <WrappedNavBar />
       <Routes>
-        <Route path="/" element={<WrappedHomePage />} />
+        <Route path="/" element={<WrappedMainPage />} />
+        <Route path="/doc" element={<WrappedDocPage />} />
         <Route path="/about" element={<WrappedAboutPage />} />
         <Route path="*" element={<WrappedNotFound />} />
       </Routes>
