ec8295bd3d0d40201ab40910960f06b117cb4a1f
dashygo097 <dashygo097@github.com>
2025-09-08 02:04:13 +0800

    usb detection ok

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,51 +5,101 @@ const createMainWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 1000,
     height: 750,
-    frame: true,
-    transparent: false,
-    backgroundMaterial: "mica",
     webPreferences: {
-      sandbox: true,
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: false,
+      contextIsolation: true,
     },
   });
 
+  let usbDeviceLists: Electron.USBDevice[] = [];
+  let grantedDeviceThroughPermHandler: Electron.USBDevice;
+
   mainWindow.webContents.session.on(
-    "select-serial-port",
-    (event, portList, webContents, callback) => {
-      mainWindow.webContents.session.on("serial-port-added", (event, port) => {
-        console.log("Serial port added:", port);
-      });
-      mainWindow.webContents.session.on(
-        "serial-port-removed",
-        (event, port) => {
-          console.log("Serial port removed:", port);
-        },
-      );
+    "select-usb-device",
+    (event, details, callback) => {
+      console.log("select-usb-device triggered:", details);
+      usbDeviceLists = details.deviceList;
 
       event.preventDefault();
-      if (portList && portList.length > 0) {
-        callback(portList[0].portId);
+
+      if (details.deviceList && details.deviceList.length > 0) {
+        const deviceToReturn = details.deviceList.find((device) => {
+          return (
+            !grantedDeviceThroughPermHandler ||
+            device.deviceId !== grantedDeviceThroughPermHandler.deviceId
+          );
+        });
+
+        if (deviceToReturn) {
+          callback(deviceToReturn.deviceId);
+        } else {
+          callback();
+        }
       } else {
-        callback("");
+        callback();
       }
     },
   );
 
+  mainWindow.webContents.session.on("usb-device-added", (event, device) => {
+    console.log("Detected new USB device:", device);
+    usbDeviceLists.push(device);
+  });
+
+  mainWindow.webContents.session.on("usb-device-removed", (event, device) => {
+    console.log("USB device removed:", device);
+    usbDeviceLists = usbDeviceLists.filter(
+      (d) => d.deviceId !== device.deviceId,
+    );
+  });
+
   mainWindow.webContents.session.setPermissionCheckHandler(
     (webContents, permission, requestingOrigin, details) => {
-      if (permission === "serial" && details.securityOrigin === "file:///") {
-        return true;
+      if (permission === "usb") {
+        const validOrigins = [
+          "file:///",
+          MAIN_WINDOW_VITE_DEV_SERVER_URL,
+        ].filter(Boolean);
+
+        return validOrigins.some(
+          (origin) =>
+            requestingOrigin.startsWith(origin) ||
+            details?.securityOrigin?.startsWith(origin),
+        );
       }
       return false;
     },
   );
 
   mainWindow.webContents.session.setDevicePermissionHandler((details) => {
-    if (details.deviceType === "serial" && details.origin === "file://") {
-      return true;
+    if (details.deviceType === "usb") {
+      const validOrigins = [
+        "file://",
+        MAIN_WINDOW_VITE_DEV_SERVER_URL?.replace(/\/$/, ""),
+      ].filter(Boolean);
+
+      const isValidOrigin = validOrigins.some((origin) =>
+        details.origin.startsWith(origin),
+      );
+
+      if (isValidOrigin) {
+        if (!grantedDeviceThroughPermHandler) {
+          grantedDeviceThroughPermHandler = details.device;
+          return true;
+        }
+        return (
+          grantedDeviceThroughPermHandler.deviceId === details.device.deviceId
+        );
+      }
     }
     return false;
+  });
+
+  mainWindow.webContents.session.setUSBProtectedClassesHandler((details) => {
+    return details.protectedClasses.filter((usbClass) => {
+      return usbClass.indexOf("audio") === -1;
+    });
   });
 
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
@@ -61,6 +111,7 @@ const createMainWindow = () => {
   }
 
   mainWindow.webContents.openDevTools();
+  return mainWindow;
 };
 
 app.on("ready", () => {
