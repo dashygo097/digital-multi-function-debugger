380ca1b37f8fdd4ea441bc86932bdafccd696548
dashygo097 <dashygo097@gmail.com>
2025-09-09 15:23:49 +0800

    eslint ok

--- frontend/vite.renderer.config.ts
+++ frontend/vite.renderer.config.ts
@@ -1,4 +1,13 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@components": resolve("./src/components"),
+      "@utils": resolve("./src/utils"),
+      "@styles": resolve("./src/styles"),
+    },
+  },
+});
