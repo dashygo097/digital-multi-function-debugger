7b4d9de13f53dde6019c3d03a4b375ff4fc57cb9
dashygo097 <dashygo097@github.com>
2025-09-18 16:05:03 +0800

    analog waveform widget ok

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,5 +1,5 @@
 export * from "./Button";
-export * from "./Waveform";
+export * from "./AnalogWaveformChart";
 export * from "./GithubIcon";
 export * from "./SelectList";
 export * from "./InputTracer";
--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 import { WithRouter, WithRouterProps } from "@utils";
-import { USBDeviceList, SerialPortList, DataPoint } from "@components";
+import {
+  USBDeviceList,
+  SerialPortList,
+  AnalogSignalData,
+  AnalogWaveformChart,
+} from "@components";
 import "@styles/main.css";
 
-const MAX_DATA_POINTS = 100;
+const MAX_DATA_POINTS = 10;
 
 interface MainPageState {
-  data: DataPoint[];
+  data: AnalogSignalData[];
   inputValue: string;
 }
 
@@ -20,48 +25,13 @@ class MainPage extends React.Component<WithRouterProps, MainPageState> {
     };
   }
 
-  componentDidMount() {
-    const initialData: DataPoint[] = [];
-    for (let i = 0; i < 20; i++) {
-      initialData.push({ x: i, y: Math.sin(i / 3) * 10 + 10 });
-    }
-    this.setState({ data: initialData });
-  }
-
-  handleInputChange = (value: string) => {
-    this.setState({ inputValue: value });
-  };
-
-  handleInputSubmit = () => {
-    const value = parseFloat(this.state.inputValue);
-    if (!isNaN(value)) {
-      this.addDataPoint(value);
-      this.setState({ inputValue: "" });
-    }
-  };
-
-  addDataPoint = (yValue: number) => {
-    this.setState((prevState) => {
-      const newX = prevState.data.length + 1;
-
-      const newDataPoint: DataPoint = { x: newX, y: yValue };
-
-      let newData = [...prevState.data, newDataPoint];
-
-      if (newData.length > MAX_DATA_POINTS) {
-        newData = newData.slice(newData.length - MAX_DATA_POINTS);
-      }
-
-      return { data: newData };
-    });
-  };
-
   render() {
     return (
       <div className="main-page">
         <h1>Control Panel</h1>
         <USBDeviceList className="main-usblist" />
         <SerialPortList className="main-seriallist" />
+        <AnalogWaveformChart data={this.state.data} />
       </div>
     );
   }
