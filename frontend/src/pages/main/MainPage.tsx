88d29d24e9b7ad3af5db39e34048c0648edad5eb
dashygo097 <dashygo097@github.com>
2025-10-11 19:49:09 +0800

    digital signal 01 ok

--- frontend/src/pages/main/MainPage.tsx
+++ frontend/src/pages/main/MainPage.tsx
@@ -6,6 +6,8 @@ import {
   SerialPortList,
   AnalogSignalData,
   AnalogWaveformChart,
+  DigitalSignalData,
+  DigitalWaveformChart,
 } from "@components";
 import "@styles/main.css";
 
@@ -26,12 +28,32 @@ class MainPage extends React.Component<WithRouterProps, MainPageState> {
   }
 
   render() {
+    const sampleData: DigitalSignalData[] = [
+      { time: 0, value: 1 },
+      { time: 1, value: 1 },
+      { time: 2, value: 1 },
+      { time: 3, value: 0 },
+      { time: 4, value: 0 },
+      { time: 5, value: 1 },
+      { time: 6, value: 0 },
+      { time: 7, value: 1 },
+      { time: 8, value: 1 },
+      { time: 9, value: 0 },
+    ];
     return (
       <div className="main-page">
         <h1>Control Panel</h1>
         <USBDeviceList className="main-usblist" />
         <SerialPortList className="main-seriallist" />
         <AnalogWaveformChart data={this.state.data} />
+        <DigitalWaveformChart
+          data={sampleData}
+          width={600}
+          height={150}
+          strokeColor="#ff6b6b"
+          backgroundColor="#1a1a1a"
+          className="my-waveform"
+        />
       </div>
     );
   }
